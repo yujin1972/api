7b8e89c87e4d1904c24e28efde1e957646e2929f
Eric Wahlforss <eric@soundcloud.com>
2009-03-03 20:00:58 +0100

    adding crossdomain swf url

--- javascript/player/players.js
+++ javascript/player/players.js
@@ -1,7 +1,7 @@
 soundManager.flashVersion = 9;
 soundManager.debugMode = false; // disable debug mode
 soundManager.defaultOptions.multiShot = false;
-//soundManager.url = '.';
+//soundManager.url = '.'; // change to 'http://a1.soundcloud.com/swf/soundmanager2_flash9.swf' for crossdomain playback
 //soundManager.useConsole = true;
 //soundManager.consoleOnly = true;
 //soundManager.useHighPerformance = false;
